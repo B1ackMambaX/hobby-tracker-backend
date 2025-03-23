d74c5cfb28c7edc03ed486d967fef85ea28f68ce
B1ackMambaX <b1ackmambax@yandex.ru>
2025-03-23 22:25:59 +0500

    Feature: Added refresh function

--- services/userService.js
+++ services/userService.js
@@ -4,6 +4,7 @@ import tokenService from "./tokenService.js";
 import UserDto from "../dtos/userDto.js";
 import ApiError from "../exceptions/apiErrors.js";
 import tokenModel from "../models/tokenModel.js";
+import userModel from "../models/userModel.js";
 
 class UserService {
     async register(email, password) {
@@ -47,6 +48,27 @@ class UserService {
     async logout(refreshToken) {
         const token = await tokenService.removeToken(refreshToken);
         return token;
+    }
+
+    async refresh(refreshToken) {
+        if (!refreshToken) {
+            throw ApiError.UnauthorizedError();
+        }
+        const userData = tokenService.validateRefreshToken(refreshToken);
+        const tokenFromDb = await tokenService.findToken(refreshToken);
+        if (!userData || !tokenFromDb) {
+            throw ApiError.UnauthorizedError();
+        }
+
+        const user = await userModel.findById(userData.id);
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto,
+        }
     }
 }
 
