bb33cabb2bd9558d0603df1d8bc1054a7f2a0a53
B1ackMambaX <b1ackmambax@yandex.ru>
2025-03-23 20:44:33 +0500

    Feature: Added DB connection and basic folder structure

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+// Добавление middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const runServer = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+runServer();
