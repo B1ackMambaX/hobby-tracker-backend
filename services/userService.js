6a0fba0a3228f13d5adf6fe71af1f7f1c818bbec
B1ackMambaX <b1ackmambax@yandex.ru>
2025-03-23 21:53:55 +0500

    Feature: Changed error types

--- services/userService.js
+++ services/userService.js
@@ -2,13 +2,13 @@ import UserModel from "../models/userModel.js";
 import bcrypt from 'bcrypt';
 import tokenService from "./tokenService.js";
 import UserDto from "../dtos/userDto.js";
+import ApiError from "../exceptions/apiErrors.js";
 
 class UserService {
     async register(email, password) {
         const candidate = await UserModel.findOne({email});
         if (candidate !== null) {
-            console.log(candidate);
-            throw new Error("User already exists");
+            throw ApiError.BadRequestError("User already exists");
         }
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await UserModel.create({email, password: hashedPassword});
