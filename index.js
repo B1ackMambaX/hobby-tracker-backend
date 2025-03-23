dd1d60a6387b462984ee5b658681cdcdc30d44c6
B1ackMambaX <b1ackmambax@yandex.ru>
2025-03-23 21:48:57 +0500

    Feature: Added errors middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import router from './router/index.js';
+import errorMiddleware from './middlewares/errorMiddleware.js';
 
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use('/api', router);
+app.use(errorMiddleware)
 
 const runServer = async () => {
     try {
