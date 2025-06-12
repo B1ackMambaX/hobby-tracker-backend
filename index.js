862dc6f841c673ab1e93c9635c92ed5ca9bad214
B1ackMambaX <b1ackmambax@yandex.ru>
2025-06-12 12:32:08 +0500

    Feature: Added CD pipeline

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ import swaggerDocs from './swagger.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
+const DB_USER = process.env.MONGO_INITDB_ROOT_USERNAME;
+const DB_PASS = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const DB_HOST = process.env.DB_URL;
+const IS_DEV = process.env.IS_DEV;
+const DEV_DB = process.env.DEV_DB;
 const app = express();
 
 // Добавление middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.APP_URL,
     credentials: true,
 }));
 swaggerDocs(app);
@@ -25,7 +30,7 @@ app.use(errorMiddleware);
 
 const runServer = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect(IS_DEV ? DEV_DB : `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:27017/cloHelper?authSource=admin`);
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (e) {
         console.log(e);
